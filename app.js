113ed1e574b35e3225f36e108c383c46d6dc58c7
Russell Gooday <rpg_digital@yahoo.co.uk>
2022-03-12 23:11:52 +0000

    port change

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const routes = require('./routes/index')
 const ejs = require('ejs')
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT || 8080
 
 app.set('view engine', 'ejs')
 
