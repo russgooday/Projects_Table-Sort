d9ad95aca5c6429fcff4f260f5f43f9cf8cecb20
Russell Gooday <rpg_digital@yahoo.co.uk>
2022-03-12 23:04:52 +0000

    update

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const { fetchTable } = require('../controllers/index')
 
 router.get('/', async (request, result) => {
-    const people = await fetchTable('people.json', 'Api-Key')
+    const people = await fetchTable('people.json', '2eab1a30')
 
     result.render('index', { people })
 })
